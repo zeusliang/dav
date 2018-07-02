9c9cb9e6e8fbea9f0a97d9f4ed43ee5429667c52
zeusliang <1687440021@qq.com>
2018-07-02 09:28:52 -0400

    new added docker od rpm install scripts

---
+++ install-docker-from-rpm-pkg.sh
@@ -0,0 +1,6 @@
+echo "starting install docker"
+sudo  yum -y install https://download.docker.com/linux/centos/7/x86_64/stable/Packages/docker-ce-selinux-17.03.2.ce-1.el7.centos.noarch.rpm
+sudo yum -y install https://download.docker.com/linux/centos/7/x86_64/stable/Packages/docker-ce-17.03.0.ce-1.el7.centos.x86_64.rpm 
+sudo systemctl enable docker
+sudo systemctl start docker 
+echo "finished docker"
